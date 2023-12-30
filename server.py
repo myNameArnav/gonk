b9e595b7777ffbf8e9ba34919a87bd5e032929f9
Arnav Jain <arnjain3@gmail.com>
2023-12-30 22:20:16 +0530

    added flask cors

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 from db import insert_data_network, show_network_history, show_filtered_history_data
 from speedtest import measure_download_speed, measure_upload_speed
 from time import time
 
 app = Flask(__name__)
 
+CORS(app)
 
 @app.route("/")
 def index():
@@ -30,4 +32,4 @@ def show_filtered_history(from_date = None, to_date = None):
     return show_filtered_history_data(from_date, to_date)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=8888)
+    app.run(debug=True, port=8888)
